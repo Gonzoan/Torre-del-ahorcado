2a9bda9752bce93407a812210892cb9bf74f7938
Gonzoan <dugonzalezr@unal.edu.co>
2026-06-20 22:17:50 -0500

    Se completo la logica del juego, tambien los niveles,sin embargo falta mejorar la imagen del interfaz

--- juego.py
+++ juego.py
@@ -1,4 +1,5 @@
 import random
+from Munecoahorcado import Animacion_muneco
 class Ahorcado:
 
     def __init__(self):
@@ -7,7 +8,10 @@ class Ahorcado:
         self.categoria = ""
         self.nivel = 1
         self.palabra = ""
-        self.vida = 6
+        self.vida = 9
+        self.letras_adivinadas = []
+        self.letras_intentadas = []
+
     def cargar_palabras(self, categoria, nivel, idioma):
     
         self.categoria = categoria  
@@ -17,7 +21,77 @@ class Ahorcado:
     
         with open(ruta, "r", encoding="utf-8") as archivo:
             palabras = archivo.read().splitlines()
-        
-        self.palabra = random.choice(palabras)
-        
+        #Pasa las palabras a mayusculas
+        self.palabra = random.choice(palabras).upper()
+        #Resetea todas las listas para el siguiente nivel
+        self.letras_adivinadas = []
+        self.letras_intentadas = []
+        self.vida = 9
+
+        #Algunas palabras tienen espacios, se agregan de una vez
+        if " " in self.palabra:
+            self.letras_adivinadas.append(" ")
     
+    def Mostrar_palabra(self):
+        #Esta funcion mostrar por pantalla la palabra oculta en ______
+        palabra_oculta = ""
+        #Recoremos la palabra letra por letra 
+
+        for letra in self.palabra:
+            if letra in self.letras_adivinadas:
+                #Si el jugador adivina la letra esta se imprime en pantalla
+                palabra_oculta += letra + " "
+            elif letra == " ":
+                #Si tiene un espacio que se muestre en pantalla
+                palabra_oculta += "   "
+            else:
+                #Si no hay ninguna adivinada todas son "___"
+                palabra_oculta += "_ "
+        
+        #Retornar la palabra sin espacio en blanco final
+        return palabra_oculta.strip()
+    
+    def jugar(self):
+        print("\tInicia el juego!")
+        #ciclo de juego donde el jugador adivina la palabra
+        while self.vida > 0 and "_" in self.Mostrar_palabra():
+            print("")
+            print(Animacion_muneco(9 - self.vida))
+            print(f"Palabra: {self.Mostrar_palabra()}")#La palabra actual
+            print(f"Vidas restantes: {self.vida}") # Las vidas restantes
+            print (f"Letras intentadas: {','.join(self.letras_intentadas)}")# Muestra las letras intentadas
+            #Menu donde se muestra las estadisticas actuales del juego
+
+
+            #Pide la letra y la convierte en mayuscula
+            letra_jugador = input("\nIntroduce una letra: ").upper()
+
+            #Comprueba que la letra sea solo una y no acepta frases largas
+            if len(letra_jugador) != 1 or not letra_jugador.isalpha():
+                print("Letra no valida, por favor vuelva a introducir la letra")
+                continue
+
+            #Si se ingresa una letra ya introducidad anterior mente se le da otra oportunidad
+            if letra_jugador in self.letras_intentadas:
+                print(f"Ya intentaste esta letra antes")
+                continue
+
+            #Se agrega la letra nueva a la lista
+            self.letras_intentadas.append(letra_jugador)
+
+            #Comprueba que la letra este en la palabra aleatoria
+            if letra_jugador in self.palabra:
+                print("Acertaste!!")
+                self.letras_adivinadas.append(letra_jugador)
+            else:
+                #Si falla la animacion cambia
+                print("Fallaste!!")
+                self.vida -= 1
+        #Si no pierde todas las vidas pada al siguiete nivel
+        if self.vida > 0:
+            print(Animacion_muneco(9 - self.vida))
+            print(f"Nivel {self.nivel} completado!!\nLa palabra era: {self.palabra}")
+        else:
+            #Pierde totalmente
+            print(Animacion_muneco(9))
+            print(f"Has sido ahorcado en el nivel {self.nivel}.\nLa palabra era: {self.palabra}")
--- main.py
+++ main.py
@@ -5,9 +5,13 @@ seleccion = 0
 
 if seleccion == 0:
 
-    print("I---- bienvenido a la torre del ahorcado ----I")
+    print("""
+======================================
+|  BIENVENIDO AL JUEGO DEL AHORCADO  |
+======================================
+""")
     print(" ")
-    seleccion = int(input("            escriba 1 para empezar: "))
+    seleccion = int(input("     Escriba 1 para empezar: "))
     print(" ")
 
 if seleccion == 1:
@@ -20,7 +24,28 @@ if seleccion == 1:
 
     juego = Ahorcado()
     juego.cargar_palabras(categoria, 1,Idioma) #llama la logica de selección de las palabras
-    print(juego.palabra) #prueba de que si sirva la función y la muestre en consola
+    #print(juego.palabra) #prueba de que si sirva la función y la muestre en consola
+    #En caso de completar todo el juego
+    victoria_total = True
 
+    #Los niveles del 1 al 5
+    for nivel_actual in range (1,6):
+        print("\n" + "="*35)
+        print(f"    CARGANDO: NIVEL {nivel_actual} DE 5")
+        print("="*35)
+        #Cargamos la nueva palabra del nuevo nivel
+        juego.cargar_palabras(categoria, nivel_actual, Idioma)
+        #Iniciamos la logica del juego del nuevo nivel y palabra
+        juego.jugar()
+        
+        if juego.vida == 0:
+            #Si se quedo sin vidas, ya se acaba todo el juego
+            print(f"Fin del juego. Te quedaste en el nivel {nivel_actual}")
+            break
 
+        if nivel_actual < 5:
+            print("Cargando el siguiente nivel...")
+            input("Presiona Enter para continuar")
 
+    if victoria_total:
+        print("Pasaste todos los niveles del ahorcado")
