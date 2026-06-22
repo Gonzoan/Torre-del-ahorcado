bfccf074212e8813fc2ed638f99d92fa3a686510
Salomon Jarro <chovengo2018@gmail.com>
2026-06-22 14:12:12 -0500

    se añadio la posibilidad para que el usuario pueda escribir su nombre y se utilice tanto al perder como al ganar,y se renombro el archivo de jugador ya que no se usaba y se utilizará para implementar otra función más tarde

--- main.py
+++ main.py
@@ -1,54 +1,48 @@
 from juego import Ahorcado
 from MenuSeleccionTema import SelectorTema, SelectorIdioma
 
-seleccion = 0
-
-if seleccion == 0:
-
-    print("""
+print("""
 ======================================
 |  BIENVENIDO AL JUEGO DEL AHORCADO  |
 ======================================
 """)
-    print(" ")
-    seleccion = int(input("     Escriba 1 para empezar: "))
-    print(" ")
 
-if seleccion == 1:
+nombre_jugador = input("Escriba su nombre para empezar: ")
+print(f"\n¡Bienvenido, {nombre_jugador}!\n")
 
-    selector = SelectorTema()
-    categoria = selector.SeleccionTematicas() #funcion para selecciónar las tematicas, viene de MenuSeleccionTema.py
+selector = SelectorTema()
+categoria = selector.SeleccionTematicas()
 
-    SelectorIdioma = SelectorIdioma()
-    Idioma = SelectorIdioma.SeleccionIdioma(categoria) #funcion para seleccionar el idioma, tambien viene de MenuSeleccionTema.py
+selector_idioma = SelectorIdioma()
+Idioma = selector_idioma.SeleccionIdioma(categoria)
 
-    juego = Ahorcado()
-    juego.cargar_palabras(categoria, 1,Idioma) #llama la logica de selección de las palabras
-    #print(juego.palabra) #prueba de que si sirva la función y la muestre en consola
-    #En caso de completar todo el juego
-    victoria_total = True
+juego = Ahorcado()
+juego.cargar_palabras(categoria, 1,Idioma) #llama la logica de selección de las palabras
+#print(juego.palabra) #prueba de que si sirva la función y la muestre en consola
+#En caso de completar todo el juego
+victoria_total = True
 
-    #Los niveles del 1 al 5
-    for nivel_actual in range (1,6):
-        print("\n" + "="*35)
-        print(f"    CARGANDO: NIVEL {nivel_actual} DE 5")
-        print("="*35)
-        #Cargamos la nueva palabra del nuevo nivel
-        juego.cargar_palabras(categoria, nivel_actual, Idioma)
-        #Iniciamos la logica del juego del nuevo nivel y palabra
-        juego.jugar()
-        
-        if juego.vida == 0:
-            #Si se quedo sin vidas, ya se acaba todo el juego
-            print(f"Fin del juego. Te quedaste en el nivel {nivel_actual}")
-            victoria_total = False
-            break
+#Los niveles del 1 al 5
+for nivel_actual in range (1,6):
+    print("\n" + "="*35)
+    print(f"    CARGANDO: NIVEL {nivel_actual} DE 5")
+    print("="*35)
+    #Cargamos la nueva palabra del nuevo nivel
+    juego.cargar_palabras(categoria, nivel_actual, Idioma)
+    #Iniciamos la logica del juego del nuevo nivel y palabra
+    juego.jugar()
 
-        if nivel_actual < 5:
-            print("Cargando el siguiente nivel...")
-            input("Presiona Enter para continuar")
+    if juego.vida == 0:
+        #Si se quedo sin vidas, ya se acaba todo el juego
+        print(f"{nombre_jugador} llegó hasta el nivel {nivel_actual}.")
+        victoria_total = False
+        break
 
-    if victoria_total:
-        print("=======================================================")
-        print("Enhorabuena!! Pasaste todos los niveles del ahorcado!!")
-        print("=======================================================")
+    if nivel_actual < 5:
+        print("Cargando el siguiente nivel...")
+        input("Presiona Enter para continuar")
+
+if victoria_total:
+    print("=======================================================")
+    print(f"¡Felicitaciones {nombre_jugador}! Completaste todos los niveles.!!")
+    print("=======================================================")
